ae6e389f397e2ca643f6bd44e16df8a564e6ba5c
mixassio <mixail.bondarev@gmail.com>
2019-10-12 20:17:18 +0300

    setting lint prettier etc

---
+++ Makefile
@@ -0,0 +1,14 @@
+S := app
+
+PROJECT := workShop_infra
+
+include make-app.mk
+
+ansible-vaults-encrypt:
+	ansible-vault encrypt ansible/production/group_vars/all/vault.yml
+
+ansible-vaults-decrypt:
+	ansible-vault decrypt ansible/production/group_vars/all/vault.yml
+
+ansible-vaults-edit:
+	ansible-vault edit ansible/production/group_vars/all/vault.yml
---
+++ make-app.mk
@@ -0,0 +1,17 @@
+development-setup-env:
+	ansible-playbook ansible/development.yml -i ansible/development -vv
+
+app:
+	docker-compose up
+
+app-build:
+	docker-compose build
+
+app-bash:
+	docker-compose run app bash
+
+app-setup: app-build
+	docker-compose run app npm install
+	docker-compose run app npm run webpack -- -p --env production
+	docker-compose run app npm run sequelize db:migrate
+	ansible-playbook ansible/development.yml -i ansible/development -vv
